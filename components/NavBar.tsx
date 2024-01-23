f1433f8afdc8f75a8500dbdfc78b7c801c96556b
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2024-01-23 15:03:39 +0200

    working on navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { mainLinks } from "@/constants";
+import { userLinks } from "@/constants";
+import { User } from "@prisma/client";
+
+//icons
+import {
+  AiOutlineUser,
+  AiOutlineShoppingCart,
+  AiOutlineHeart,
+} from "react-icons/ai";
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+import { TbBracketsAngle } from "react-icons/tb";
+
+const NavBar = () => {
+  return (
+    <div>NavBar lol</div>
+  )
+}
+
+export default NavBar
